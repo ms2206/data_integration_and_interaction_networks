aec697e83e1e32d7a08b981750354421f24fed7c
Matthew Spriggs <matspriggs@gmail.com>
2025-02-10 16:18:21 +0000

    add server.js for Express application and remove script.js

---
+++ mon/nodejs_app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app - express();
+
+const PORT = 3000;
+const pasth_to_project = '/Users/mspriggs/Library/CloudStorage/OneDrive-Illumina,Inc./Documents/Applied_Bioinformatics/modules/data_integration_and_interaction_networks/mon/nodejs_app';
+app.use(express.static(path_to_project));
+
+app.listen(PORT, function() {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
