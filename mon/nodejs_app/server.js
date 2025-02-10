42bc3972da66fcbc6985f313191dcab106f5a3b2
Matthew Spriggs <matspriggs@gmail.com>
2025-02-10 16:33:15 +0000

    fix syntax error and add /messages endpoint to server.js

--- mon/nodejs_app/server.js
+++ mon/nodejs_app/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const app - express();
+const app = express();
 
 const PORT = 3000;
-const pasth_to_project = '/Users/mspriggs/Library/CloudStorage/OneDrive-Illumina,Inc./Documents/Applied_Bioinformatics/modules/data_integration_and_interaction_networks/mon/nodejs_app';
+const path_to_project = '/Users/mspriggs/Library/CloudStorage/OneDrive-Illumina,Inc./Documents/Applied_Bioinformatics/modules/data_integration_and_interaction_networks/mon/nodejs_app';
 app.use(express.static(path_to_project));
+
+app.get('/messages', function(require, response) {
+    response.json(messages);
+});
+
 
 app.listen(PORT, function() {
     console.log(`Server is running on http://localhost:${PORT}`);
