a1d1a16627c42d821f0d042a994eb071d0390027
Matthew Spriggs <matspriggs@gmail.com>
2025-02-10 15:51:28 +0000

    added mon

---
+++ mon/js_prac/script.js
@@ -0,0 +1,40 @@
+
+function sendMessage(){
+    const input_elem = document.getElementById('user-name-input');
+    const user_name = input_elem.value;
+    const input_message_elem = document.getElementById('message-input');     
+    const input_message = input_message_elem.value;
+    messages.push({
+        author: user_name,
+        messages: input_message
+    });
+    displayMessages();
+    const elem = document.getElementById('greeting-area');
+}
+
+
+const messages = [
+    {
+        author: 'Matthew',
+        messages: 'Hello'
+    },
+    {
+        author: 'Jo',
+        messages: 'Hi'
+    }
+];
+
+
+
+
+function displayMessages() {
+    const container = document.getElementById('message-container');
+    container.innerHTML = ''; // clear contents
+    for (let i = 0; i < messages.length; i++) {
+        const elem = document.createElement('div');
+        const msg = `<b>${messages[i].author}</b> says: ${messages[i].messages}`
+        elem.innerHTML = msg;
+        container.appendChild(elem);
+    }
+}
+displayMessages();
