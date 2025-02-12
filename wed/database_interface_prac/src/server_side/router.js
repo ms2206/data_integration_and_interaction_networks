00df6824e94e882ddb2b427dbb43184c69aa6968
Matthew Spriggs <matspriggs@gmail.com>
2025-02-12 15:26:54 +0000

    feat: add endpoints to retrieve authors, experiments, datapoints, and organisms

--- wed/database_interface_prac/src/server_side/router.js
+++ wed/database_interface_prac/src/server_side/router.js
@@ -4,11 +4,13 @@ const db = new sqlite3.Database('../microdb.sqlite');
 
 
 const micobe_router = express.Router();
+
 micobe_router.use(function(req, res, next) {
     console.log('Recieved at Time:', Date.now());
     next();
 });
 
+// make endpoint for getting all authors
 micobe_router.get('/authors', function(req, res) {
     const query = 'SELECT * FROM authors';
     db.all(query, [], function(err, rows) {
@@ -20,6 +22,41 @@ micobe_router.get('/authors', function(req, res) {
     });
 });
 
+// make endpoint for getting all experiments
+micobe_router.get('/experiments', function(req, res) {
+    const query = 'SELECT * FROM experiments';
+    db.all(query, [], function(err, rows) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// make endpoint for getting all datapoints
+micobe_router.get('/datapoints', function(req, res) {
+    const query = 'SELECT * FROM datapoints';
+    db.all(query, [], function(err, rows) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// make endpoint for organisms
+micobe_router.get('/organisms', function(req, res) {
+    const query = 'SELECT * FROM organisms';
+    db.all(query, [], function(err, rows) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        res.json(rows);
+    });
+});
 
 
 module.exports = micobe_router;
