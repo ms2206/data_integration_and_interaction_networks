05520b2c4131ca9cfe7237dfc26de1ad5c260ccd
Matthew Spriggs <matspriggs@gmail.com>
2025-02-12 16:29:04 +0000

    refactor: remove server-side rendering files and configurations

--- wed/angular_dev/first-practical/src/app/app.module.ts
+++ wed/angular_dev/first-practical/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
